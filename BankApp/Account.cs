dbc06ff857bbaa8ab3c2671461deffec5496f300
Kal Academy <info@kalacademy.org>
2017-07-15 10:24:02 -0700

    Initial push

---
+++ BankApp/Account.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    enum TypeOfAccounts
+    {
+        Checking,
+        Savings,
+        Loan,
+        CD
+    }
+    /// <summary>
+    /// This class describes a bank account
+    /// </summary>
+    class Account
+    {
+        #region statics
+        private static int lastAccountNumber = 0;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Email address of the 
+        /// person holding the account
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        public int AccountNumber { get; private set; }
+
+        public int SSN { get; set; }
+
+        public DateTime CreatedDate { get; private set; }
+
+        public TypeOfAccounts AccountType { get; set; }
+
+        public decimal Balance { get; private set; }
+
+        #endregion
+
+        #region constructors
+        public Account()
+        {
+            //lastAccountNumber = lastAccountNumber + 1;
+            AccountNumber = ++lastAccountNumber;
+            CreatedDate = DateTime.Now;
+        }
+
+        //public Account(string emailAddress) : this()
+        //{
+        //    EmailAddress = emailAddress;
+        //}
+
+        //public Account(string emailAddress, TypeOfAccounts accountType) : this(emailAddress)
+        //{
+        //    AccountType = accountType;
+        //}
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        /// <summary>
+        /// Withdraw money from the bank
+        /// </summary>
+        /// <param name="amount">Amount to withdraw</param>
+        /// <returns>The new balance</returns>
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account
+            {
+                EmailAddress = "test@test.com",
+                AccountType = TypeOfAccounts.Checking
+            };
+
+            myAccount.Deposit(500.01M);
+
+            Console.WriteLine($"AccountNumber: {myAccount.AccountNumber}, Account Type: {myAccount.AccountType}, Balance: {myAccount.Balance:C}, CreatedDate: {myAccount.CreatedDate}");
+
+            var myAccount2 = new Account
+            {
+                AccountType = TypeOfAccounts.Savings
+            };
+            myAccount2.EmailAddress = "test2@test.com";
+            //myAccount2.AccountType = TypeOfAccounts.Savings;
+            Console.WriteLine($"AccountNumber: {myAccount2.AccountNumber}, Account Type: {myAccount2.AccountType}, Balance: {myAccount2.Balance:C}, CreatedDate: {myAccount2.CreatedDate}");
+
+        }
+    }
+}
